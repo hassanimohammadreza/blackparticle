55e671f66e5710b2f8308c91a256cef4ab3b3965
Mohammadreza Hassani <252051910+hassanimohammadreza@users.noreply.github.com>
2026-02-03 09:19:15 +0330

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,45 @@
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.navbar-links');
+
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+  
+
+// const toggleTheme = document.getElementById('themeToggle');
+
+// toggleTheme.addEventListener('click', () => {
+//   document.body.classList.toggle('light');
+// });
+
+
+
+const form = document.getElementById("contactForm");
+
+const formMessage = document.createElement("div");
+formMessage.classList.add("form-message");
+form.parentNode.insertBefore(formMessage, form.nextSibling);
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  formMessage.textContent = "";
+  formMessage.classList.remove("success", "error");
+
+  const formData = new FormData(form);
+
+  fetch("https://script.google.com/macros/s/AKfycbyoMLOlQs3BtRLkKs-m2XECblWf-IPwg8Na2JqWUtUP_K0AHYjWw7uXqfwtbeQ1kSq7/exec", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.text())
+    .then(() => {
+      formMessage.textContent = "Your message has been successfully sent.";
+      formMessage.classList.add("success");
+      form.reset();
+    })
+    .catch(() => {
+      formMessage.textContent = "An error occurred while sending your message. Please try again.";
+      formMessage.classList.add("error");
+    });
+});
